ecb086bab22554137a96a5f982b156bcf897017e
galalkoro98 <galal.koro@gmail.com>
2025-05-29 02:03:40 +0200

    updated

--- src/shared/utils/baseURL.ts
+++ src/shared/utils/baseURL.ts
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const baseURL = process.env.NEXT_PUBLIC_API_URL;
-export const baseURL = "http://localhost:5000";
+export const baseURL = process.env.NEXT_PUBLIC_API_URL;
+// export const baseURL = "http://localhost:5000";
